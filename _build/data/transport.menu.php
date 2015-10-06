c4c27d53d37047792676b16e81a39d50dc77faf5
Florian Gutwald <florian@frontend-mercenary.com>
2015-10-07 01:18:05 +0200

    Optimize transport menu

--- _build/data/transport.menu.php
+++ _build/data/transport.menu.php
@@ -6,24 +6,13 @@
 * @subpackage build
 */
 $action= $modx->newObject('modAction');
-$action->fromArray(array(
-    'id' => 1,
-    'namespace' => 'seaccelerator',
-    'parent' => 0,
-    'controller' => 'home',
-    'haslayout' => true,
-    'lang_topics' => 'seaccelerator.:default,lexicon',
-    'assets' => '',
-),'',true,true);
 
 /* load action into menu */
 $menu= $modx->newObject('modMenu');
 $menu->fromArray(array(
     'text' => 'seaccelerator.title',
-    'parent' => 'seaccelerator',
+    'parent' => 'components',
     'description' => 'seaccelerator.description',
-    'icon' => 'images/icons/plugin.gif',
-    'menuindex' => 2,
     'params' => '',
     'handler' => '',
 ),'',true,true);
