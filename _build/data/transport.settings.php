225bc1d3bb0ab251c9e674e23704a592df405be8
Florian Gutwald <florian@frontend-mercenary.com>
2015-10-07 11:25:44 +0200

    Optimize transport settings.

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -15,27 +15,34 @@ $settings = array();
 /* The first three are new settings */
 $settings['seaccelerator.elements_directory']= $modx->newObject('modSystemSetting');
 $settings['seaccelerator.elements_directory']->fromArray(array (
-	'key' => 'seaccelerator.elements_directory',
-	'value' => '',
-	'namespace' => 'seaccelerator',
-	'area' => 'Settings',
+    'key' => 'seaccelerator.elements_directory',
+    'value' => '',
+    'namespace' => 'seaccelerator',
+    'name' => 'seaccelerator.elements_directory.name',
+    'description' => 'seaccelerator.elements_directory.description',
+    'area' => 'seaccelerator.namespace.settings',
 ), '', true, true);
 
 $settings['seaccelerator.mediasource']= $modx->newObject('modSystemSetting');
 $settings['seaccelerator.mediasource']->fromArray(array (
-	'key' => 'seaccelerator.mediasource',
-	'xtype' => 'modx-combo-source',
-	'value' => '1',
-	'namespace' => 'seaccelerator',
-	'area' => 'Settings',
+    'key' => 'seaccelerator.mediasource',
+    'xtype' => 'modx-combo-source',
+    'value' => '1',
+    'namespace' => 'seaccelerator',
+    'name' => 'seaccelerator.mediasource.name',
+    'description' => 'seaccelerator.mediasource.description',
+    'area' => 'seaccelerator.namespace.settings',
 ), '', true, true);
+
 $settings['seaccelerator.use_categories']= $modx->newObject('modSystemSetting');
 $settings['seaccelerator.use_categories']->fromArray(array (
-	'key' => 'seaccelerator.use_categories',
-	'xtype' => 'combo-boolean',
-	'value' => '1',
-	'namespace' => 'seaccelerator',
-	'area' => 'Settings',
+    'key' => 'seaccelerator.use_categories',
+    'xtype' => 'combo-boolean',
+    'value' => '1',
+    'namespace' => 'seaccelerator',
+    'name' => 'seaccelerator.use_categories.name',
+    'description' => 'seaccelerator.use_categories.description',
+    'area' => 'seaccelerator.namespace.settings',
 ), '', true, true);
 
 
